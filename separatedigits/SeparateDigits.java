00bf0e5053cb9a17ff6e416a28786f819d2d59fd
Thomas Mejia <tommym147@gmail.com>
2017-05-23 15:34:24 -0700

    finished challenege

--- separatedigits/SeparateDigits.java
+++ separatedigits/SeparateDigits.java
@@ -1,6 +1,6 @@
 // Thomas Mejia 5/23/17
-// Write an application that inputs one number consisting of five digits from the user, 
-// separates the number into its individual digits and prints the digits separated 
+// Write an application that inputs one number consisting of five digits from the user,
+// separates the number into its individual digits and prints the digits separated
 // from one another by three spaces each.
 
 import java.io.BufferedReader;
@@ -17,12 +17,17 @@ public class SeparateDigits {
 		System.out.println("Enter 5 digit integer: ");
 		try {
 			String input = console.readLine();
-			while (input.length() != 5) {
+			while (input.length() != 5) { // check if 5 digits were inputed
 				System.out.println("Invalid input. Please enter 5 digit integer.");
 				input = console.readLine();
 			}
-			int number = Integer.parseInt(input);
-			System.out.println("Input is: " + number);
+			// check if string is numeric
+			Integer.parseInt(input);
+
+			// separate digits
+			for (int i = 0; i < input.length(); i++) {
+				System.out.print(input.charAt(i) + "   ");
+			}
 		}
 		catch (IOException | NumberFormatException e) {
 			System.out.println(e);
