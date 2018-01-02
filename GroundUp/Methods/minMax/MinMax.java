6e9b35dc33f631afc1ccfad2c1bc7681f0cb9eb9
Thomas Mejia <tommym147@gmail.com>
2018-01-01 17:09:38 -0800

    added comments to minMax challenge

--- GroundUp/Methods/minMax/MinMax.java
+++ GroundUp/Methods/minMax/MinMax.java
@@ -5,14 +5,14 @@
 import java.util.Scanner;
 
 public class MinMax {
-	public static int myMin(int x, int y) {
+	public static int myMin(int x, int y) { // function that returns min value
 		if (x < y) {
 			return x;
 		}
 		return y;
 	}
 	
-	public static int myMax(int x, int y) {
+	public static int myMax(int x, int y) { // function that returns max value
 		if (x > y) {
 			return x;
 		}
@@ -20,16 +20,19 @@ public class MinMax {
 	}
 	
 	public static void main(String [] args) {
-		Scanner input = new Scanner(System.in);
-		int x, y, min, max;
+		Scanner input = new Scanner(System.in); // reads user input
+		int x, y, min, max; // x and y hold user input, min and max hold which values are min and max
 		
+		// ask for user input
 		System.out.println("Please enter two integers: ");
 		x = input.nextInt();
 		y = input.nextInt();
 		
+		// determine min and max values
 		min = myMin(x,y);
 		max = myMax(x,y);
 		
+		// print min and max
 		System.out.println("Minimum value: " + min);
 		System.out.println("Maximum value: " + max);
 	}
