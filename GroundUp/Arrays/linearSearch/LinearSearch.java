4d8eda93339836a246153931a17b5d0a382e8d2e
Thomas Mejia <tommym147@gmail.com>
2018-01-09 17:21:38 -0800

    added comments to linearSearch challenge

--- GroundUp/Arrays/linearSearch/LinearSearch.java
+++ GroundUp/Arrays/linearSearch/LinearSearch.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 public class LinearSearch {
 	public static boolean linearSearch (int [] array, int value) {
-		for (int i = 0; i < array.length; i++) {
-			if (value == array[i]) {
+		for (int i = 0; i < array.length; i++) { // iterate through whole array
+			if (value == array[i]) { // if there's a match, return true
 				return true;
 			}
 		}
@@ -14,13 +14,15 @@ public class LinearSearch {
 	}
 	
 	public static void main(String [] args) {
-		Scanner input = new Scanner(System.in);
-		int [] array = {4, 3, 5, 2, 1, 9, 45, 88, 33};
-		int value;
+		Scanner input = new Scanner(System.in); // reads user input
+		int [] array = {4, 3, 5, 2, 1, 9, 45, 88, 33}; // array that holds integer values
+		int value; // variable to hold user input
 		
+		// ask for user input
 		System.out.println("Enter integer you want to search for: ");
 		value = input.nextInt();
 		
+		// print whether integer was found
 		if (linearSearch(array, value)) {
 			System.out.println("The integer is in the array.");
 		}
